42e83982c46c6e47b1929d0457d71b706457a274
Gukin Han <gukin.dev@gmail.com>
2024-01-11 11:43:14 +0900

    순수 JPA 기반 리포지토리 만들기 end

--- src/main/java/study/data/jpa/repository/MemberJpaRepository.java
+++ src/main/java/study/data/jpa/repository/MemberJpaRepository.java
@@ -5,6 +5,9 @@ import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import study.data.jpa.entity.Member;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public class MemberJpaRepository {
 
@@ -14,6 +17,31 @@ public class MemberJpaRepository {
     public Member save(Member member) {
         em.persist(member);
         return member;
+    }
+
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
+    /**
+     * 여러건 조회 및 조건 조회는 JPQL을 이용.
+     * SQL과 비슷해보이지만 오브젝트를 대상으로 한다.
+     * JPQL -> SQL로 번역
+     */
+    public List<Member> findAll() {
+        //
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    public long count() {
+        return em.createQuery("select count(m) from Member m", Long.class)
+                .getSingleResult(); // 단건인 경우
     }
 
     public Member find(Long id) {
