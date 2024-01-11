1ff50a3f37d532c551a088b2adeeaaf387681521
Gukin Han <gukin.dev@gmail.com>
2024-01-11 13:24:16 +0900

    공통 인터페이스 설정 end

--- src/main/java/study/data/jpa/repository/TeamRepository.java
+++ src/main/java/study/data/jpa/repository/TeamRepository.java
@@ -1,41 +1,8 @@
 package study.data.jpa.repository;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import org.springframework.stereotype.Repository;
-import study.data.jpa.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
 import study.data.jpa.entity.Team;
 
-import java.util.List;
-import java.util.Optional;
-
-@Repository
-public class TeamRepository {
-
-    @PersistenceContext
-    private EntityManager em;
-
-    public Team save(Team team) {
-        em.persist(team);
-        return team;
-    }
-
-    public void delete(Team team) {
-        em.remove(team);
-    }
-
-    public List<Team> findAll() {
-        return em.createQuery("select t from Team t", Team.class)
-                .getResultList();
-    }
-
-    public Optional<Team> findById(Long id) {
-        Team team = em.find(Team.class, id);
-        return Optional.ofNullable(team);
-    }
-
-    public long count() {
-        return em.createQuery("select count(t) from Team t", Long.class)
-                .getSingleResult();
-    }
+// 리파지토리 애노테이션 생략 가능
+public interface TeamRepository extends JpaRepository<Team, Long> {
 }
