26d79a9a2e5e783a91811f890cec304ef06ccd91
Gukin Han <gukin.dev@gmail.com>
2024-01-12 15:18:42 +0900

    네이트브 쿼리 end

---
+++ src/main/java/study/data/jpa/repository/MemberProjection.java
@@ -0,0 +1,10 @@
+package study.data.jpa.repository;
+
+public interface MemberProjection {
+
+    Long getId();
+
+    String getUsername();
+
+    String getTeamName();
+}
--- src/test/java/study/data/jpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/data/jpa/repository/MemberRepositoryTest.java
@@ -376,4 +376,26 @@ class MemberRepositoryTest {
             System.out.println("usernameOnly = " + usernameOnly.getUsername());
         }
     }
+
+    @Test
+    public void nativeQuery() {
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+
+        Member m1 = new Member("m1", 0, teamA);
+        Member m2 = new Member("m2", 0, teamA);
+        em.persist(m1);
+        em.persist(m2);
+
+        em.flush();
+        em.clear();
+
+        Page<MemberProjection> result = memberRepository.findByNativeProjection(PageRequest.of(0, 10));
+        final List<MemberProjection> content = result.getContent();
+        for (MemberProjection memberProjection : content) {
+            System.out.println("memberProjection.getUsername() = " + memberProjection.getUsername());
+            System.out.println("memberProjection.getUsername() = " + memberProjection.getTeamName());
+
+        }
+    }
 }
